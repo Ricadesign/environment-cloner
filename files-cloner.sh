52f2b42c891fd9ca997837d6bb39ecc3df6cfbec
Joel Pérez <jperez@rica.design>
2024-04-01 00:55:13 +0200

    fix: copy files on destination server

--- files-cloner.sh
+++ files-cloner.sh
@@ -32,13 +32,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Copy files from the source server directly to the destination server
-echo "Copying files from $source_path on $source_server to $destination_path on $destination_server..."
-scp -r "$source_user@$source_server:$source_path" "$destination_user@$destination_server:$destination_path"
+# Copy files from the source server to the local folder
+echo "Copying files from $source_path on $source_server to ./copy..."
+rsync -avz -e ssh "$source_user@$source_server:$source_path/" "./copy/"
 
-# Check the status of the copy operation
+# Check the status of the local copy operation
 if [ $? -ne 0 ]; then
-    echo "Error: Unable to copy files from $source_server to $destination_server."
+    echo "Error: Unable to copy files from $source_server."
+    exit 1
+fi
+
+# Clone files from the local folder to the destination server
+echo "Cloning files from ./copy on the source server to $destination_path on $destination_server..."
+rsync -avz -e ssh "./copy/" "$destination_user@$destination_server:$destination_path/"
+
+rm -rf "./copy/"
+
+# Check the status of the cloning operation
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to clone files to $destination_server."
     exit 1
 fi
 
